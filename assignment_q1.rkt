df804c31e7e74bda16d47d2e69963c5ba4a9867f
Pakkeav <p.keaveney2@nuigalway.ie>
2017-10-24 10:08:55 +0100

    Q1 complete

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,12 @@
 #lang racket
+
+> (cons 5 1)
+
+> (cons 5(cons 1(cons 7 null)))
+
+> (cons (cons(cons 5(cons 1(cons 7 null))) 3)  "hey")
+
+> (list (list (list 5 1 7) 3) "hey")
+
+> (append '(((5 1 7)) '3) '("hey"))
+
