90c01b8fa13ddc43c6de17663ef29d5536ac281b
Pakkeav <p.keaveney2@nuigalway.ie>
2017-10-26 20:45:09 +0100

    Add files via upload

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,66 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
+;;A)
+(define (ins_beg lis1 lis2)
+  (cons lis1 lis2))
 
-(provide ins_beg)
+>(ins_beg 'a '(b c d))
+>(ins_beg '(a b) '(b c d))
 
-(define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+;;B)
+(define (ns_end lis1 lis2)
+  (cons lis2 lis1))
+
+>(ns_end 'a '(b c d))
+>(ns_end '(a b) '(b c d))
+
+;;C)
+ (define (cout_top_level  ls)
+  (if (null? ls)
+  0
+  (+ 1 (cout_top_level  (cdr ls)))))
+
+>(cout_top_level  '(2 1 3 6 6 3 5 7))
+
+;;D)
+(define (count_instances x ls)
+  (if (null? ls)
+   0
+     (if (equal? x (car ls))
+	(+ 1 (count_instances x (cdr ls)))
+	(count_instances x (cdr ls)))))
+
+>(count_instances  '3 '(1 3 6 6 3 3 1))
+
+;;E)
+(define (count_instances_tr x ls)
+   (count_instances_tr_1 x ls 0))
+
+(define (count_instances_tr_1 x ls num)
+ (if (null? ls) num
+    (if (equal? x (car ls))
+	 (count_instances_tr_1 x (cdr ls)(+ 1 num))
+	(count_instances_tr_1 x (cdr ls) num))))
+
+>(count_instances_tr  '3 '(1 3 6 6 3 3 1))
+
+;;F)
+(define (count_instances_deep x ls)
+   (count_instances_deep_1 x ls 0))
+
+(define (count_instances_deep_1 x ls num)
+  (cond[ (empty? ls) 0]
+    [(equal? x (car ls))  (count_instances_deep_1 x (cdr ls)(+ 1 num))]
+     [   ((list? (car ls)) ((count_instances_deep_1 x (car ls) (+ 1 num)) (count_instances_deep_1 x (cdr ls)(+ 1 num))))]
+    [else (count_instances_deep_1 x (cdr ls) num)]))
+
+>(count_instances_deep '3 '((5 3) (3 4) 7 (1 8) 3 (5 0 3 3 7 9 3 5 7 3)))
+
+
+
+
+
+
+
+
+
--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,37 @@
 #lang racket
+
+
+(define tree '(((()1())5(()8()))10((()16())18((()19())51()))))
+
+;A)
+(define (sort tree)
+   (cond[(not(empty? tree))(sort (car tree))
+     (printf" ~a "(cadr tree))
+     (sort (caddr tree))]))
+
+>(sort tree)
+
+;B)
+(define (present item tree)
+  (cond[(empty? tree)#f]
+       [(equal? item(cadr tree))#t] 
+         [(< item (cadr tree))(present item (car tree))]
+          [(> item (cadr tree))(present item (caddr tree))]))
+
+>(present 11 tree)
+
+;C)
+(define (insert item tree)
+  (cond[(empty? tree)(list empty item empty)]
+         [(< item (cadr tree))(list (insert item (car tree)) (cadr tree) (caddr tree))]
+         [(= item (cadr tree))tree]
+         [(> item (cadr tree))(list (car tree) (cadr tree) (insert item (caddr tree)))]))
+
+>(insert 2 tree)
+
+
+;D)
+(define (insert_list list tree )
+    (insert_list (insert (car list) tree)( cdr list)))
+
+>(insert_list '(2 44 5 33) tree)
